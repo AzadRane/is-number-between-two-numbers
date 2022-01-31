73bb13dcb7605d12e31305b30119f9c0547a64a3
Harish Rane <51317454+AzadRane@users.noreply.github.com>
2022-01-31 18:22:10 +0530

    Create sol.py
    completed

---
+++ sol.py
@@ -0,0 +1,13 @@
+#a is between b and c
+#coded by harish
+
+a =  int(input("enter a"))
+b =  int(input("enter b"))
+c =  int(input("enter c"))
+
+if (a>=b and a<=c):
+    print(f'{a} is in between {b} and {c}')
+elif (a<=b and a>=c):
+    print(f'{a} is in between {b} and {c}')
+else:
+     print("loolll")
